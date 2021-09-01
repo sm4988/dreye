3dc477862edf2800c3b08e09313656dc2b2d4fd3
sm4988 <Snvdmsv781375>
2021-09-01 16:35:16 -0400

    Gucky changes

--- dreye/stimuli/hyperspectral/baden_unpacking.py
+++ dreye/stimuli/hyperspectral/baden_unpacking.py
@@ -19,23 +19,3 @@ def preprocess_baden(raw_image):
     data = spectra_ue.magnitude  # (opsin, pixel)
 
     return data, wls
-
-
-def spiral_ccw(A):
-    A = np.array(A)
-    out = []
-    while A.size:
-        out.append(A[0][::-1])  # first row reversed
-        A = A[1:][::-1].T  # cut off first row and rotate clockwise
-    return np.concatenate(out)
-
-
-def base_spiral(nrow, ncol):
-    return spiral_ccw(np.arange(nrow * ncol).reshape(nrow, ncol))[::-1]
-
-
-def to_spiral(A):
-    A = np.array(A)
-    B = np.empty_like(A)
-    B.flat[base_spiral(*A.shape)] = A.flat
-    return B
